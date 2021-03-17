1336cb2c22ef6ef3c3cc136a61dd064ff70ced1c
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-03-17 19:40:16 +0100

    controller & model  & nav

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-white text-center p-5">
+    <p>© Bryan_vio</p>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+@section('content')
+<h1>Welcome</h1>
+    
+@endsection
